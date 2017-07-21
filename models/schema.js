c345cdc8f9a2b310f45c1daa0e9bb1fabc537c63
Kelvin Medina <kelvinm2423@gmail.com>
2017-07-20 21:42:01 -0400

    schema added

---
+++ models/schema.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Create Schema and Model
+
+var usersSchema = new Schema({
+	name: "Kelvin",
+	email: "kelvinm1724@gmail.com",
+	phone_number: 954-778-0357,
+	comment: "I think Donald is crazy"
+	});

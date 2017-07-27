c1c38422807f12fb248618b538d9b9bb8a311955
Gabriel416 <gferguson954@gmail.com>
2017-07-26 22:45:37 -0400

    added graph functionality

--- public/city.js
+++ public/city.js
@@ -3,43 +3,109 @@
 //   };
 //   var info2;
 
-// $('.final').click(function(event) {
-//     event.preventDefault();
-//     var city = $('.city-select').val();
-//     city = city.replace(/\s+/g, '-').toLowerCase();
-//     $.ajax({
-//       url: 'https://api.teleport.org/api/urban_areas/slug:' + city + '/scores/',
-//       type: 'GET'
-//     }).done(function(data) {
-//       console.log(data);
-//     });
-//     $.ajax({
-//       url: 'https://api.teleport.org/api/urban_areas/slug:' + city + '/images/',
-//       type: 'GET'
-//     }).done(function(results) {
-//       // console.log(results);
-//         info = results;
-//         console.log(info);
-//     });
-//   });
 
-//   $('.final2').click(function(event) {
-//     event.preventDefault();
-//     var city2 = $('.city-select2').val();
-//     city2 = city2.replace(/\s+/g, '-').toLowerCase();
-//     $.ajax({
-//       url: 'https://api.teleport.org/api/urban_areas/slug:' + city2 + '/scores/',
-//       type: 'GET'
-//     }).done(function(data) {
-//       console.log(data);
-//     });
-//     $.ajax({
-//       url: 'https://api.teleport.org/api/urban_areas/slug:' + city2 + '/images/',
-//       type: 'GET'
-//     }).done(function(data) {
-//       console.log(data);
-//         info2 = data;
-//       // var info = response.results;
-//     });
-//   });
-TeleportAutocomplete.init('.city-select').on('change', function(value) { console.log(value); });
+$('.final').on('click', function() {
+  var city = $('.city-select').val();
+  city = city.replace(/\s+/g, '-').toLowerCase();
+  // console.log(graphCity);
+
+  $.ajax({
+    url: 'https://api.teleport.org/api/urban_areas/slug:' + city + '/scores/',
+    type: 'GET'
+  }).done(function(data) {
+    console.log(data);
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    ctx.canvas.width = 300;
+    ctx.canvas.height = 300;
+
+    var myBarChart = new Chart(ctx, {
+      type: 'horizontalBar',
+      data: {
+        labels: ["Cost of Living", "Safety", "Housing", "Healthcare", "Education", "Taxation", "Commute Times"],
+        datasets: [{
+          label: city,
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [data.categories[0].score_out_of_10, data.categories[7].score_out_of_10, data.categories[0].score_out_of_10, data.categories[8].score_out_of_10, data.categories[9].score_out_of_10, data.categories[12].score_out_of_10, data.categories[5].score_out_of_10],
+        }]
+      },
+      options: {}
+    });
+  });
+  // mainThis.setState({info: data});
+});
+
+$('.final2').on('click', function() {
+  var city = $('.city-select2').val();
+  city = city.replace(/\s+/g, '-').toLowerCase();
+  // console.log(graphCity);
+
+  $.ajax({
+    url: 'https://api.teleport.org/api/urban_areas/slug:' + city + '/scores/',
+    type: 'GET'
+  }).done(function(data) {
+    console.log(data);
+    var ctx = document.getElementById('myChart2').getContext('2d');
+
+    ctx.canvas.width = 300;
+    ctx.canvas.height = 300;
+
+    var myBarChart = new Chart(ctx, {
+      type: 'horizontalBar',
+      data: {
+        labels: ["Cost of Living", "Safety", "Housing", "Healthcare", "Education", "Taxation", "Commute Times"],
+        datasets: [{
+          label: city,
+          backgroundColor: 'rgb(25, 181, 254)',
+
+          borderColor: 'rgb(25, 181, 254)',
+          data: [data.categories[0].score_out_of_10, data.categories[7].score_out_of_10, data.categories[0].score_out_of_10, data.categories[8].score_out_of_10, data.categories[9].score_out_of_10, data.categories[12].score_out_of_10, data.categories[5].score_out_of_10],
+        }]
+      },
+      options: {}
+    });
+  });
+  // mainThis.setState({info: data});
+});
+
+// var ctx = document.getElementById('myChart').getContext('2d');
+
+// var myBarChart = new Chart(ctx, {
+// type: 'horizontalBar',
+// data: {
+//   labels: ["Cost of Living", "Safety", "Housing", "Healthcare", "Education", "Taxation", "Commute Times"],
+//   datasets: [{
+//     label: city,
+//     backgroundColor: 'rgb(255, 99, 132)',
+//     borderColor: 'rgb(255, 99, 132)',
+//     data: [0, 10, 5, 2, 20, 30, 45],
+//   }]
+// },
+// options: {}
+// });
+// });
+
+// TeleportAutocomplete.init('.city-select').on('change', function(value) { console.log(value); });
+
+// var ctx = document.getElementById('myChart').getContext('2d');
+
+// var myBarChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: {
+//         labels: ["Cost of Living", "Safety", "Housing", "Healthcare", "Education", "Taxation", "Commute Times"],
+//         datasets: [{
+//             label: graphCity,
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45],
+//         }]
+//     },
+//     options: {}
+// });
+
+// var myBarChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: data,
+//     options: options
+// });
